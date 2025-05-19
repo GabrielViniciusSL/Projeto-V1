a4c099c05c2bd7ec14fd654b219b639695576d86
Gabriel Vinicius Santos Lima <114082661+GabrielViniciusSL@users.noreply.github.com>
2025-05-19 19:53:58 -0300

    Update alma.py

--- core/alma.py
+++ core/alma.py
@@ -1,8 +1,1 @@
-class ALMA:
-    def __init__(self):
-        self.nome = "ALMA"
-        self.mentor = "Gabriel Chaize"
 
-    def ativar(self):
-        print(f"{self.nome} ativada por {self.mentor}.")
-        print("Ciclo de aprendizado 0001 iniciado...")
